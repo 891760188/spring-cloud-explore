e52b4d24ab308303a0232fffa9814bf60fdc1e5f
891760188 <891760198@qq.com>
2018-09-23 16:30:20 +0800

    rabbitMQ

--- config-client/src/main/java/com/forezp/ConfigClientApplication.java
+++ config-client/src/main/java/com/forezp/ConfigClientApplication.java
@@ -3,11 +3,15 @@ package com.forezp;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
+@RefreshScope  // 重新获取配置中心的配置  http://localhost:8881/actuator/bus-refresh
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
